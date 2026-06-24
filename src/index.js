f5ee8445e6dd590722ca51d5fd18297a63d25c87
Thiago <thiago.fernandes2003@gmail.com>
2026-06-24 12:40:44 -0300

    ajuste porta para render

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import { connectDB } from './config/db.js';
 async function startServer() {
   await connectDB(); 
 
-  app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
   });
 }
 
